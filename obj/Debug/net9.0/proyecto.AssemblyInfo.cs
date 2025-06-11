2a2f1bc17359bc65dd8bab66d449940b2fe12085
Valeria Sanchez <valeriaarenas73@gmail.com>
2025-06-10 19:16:54 -0500

    .

--- obj/Debug/net9.0/proyecto.AssemblyInfo.cs
+++ obj/Debug/net9.0/proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183505abef3c55f23ced3e78e8c910f506ccdd08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ecabcd870425e6573700ddbaf3ab50d8e27d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
